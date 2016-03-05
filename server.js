dbae1fff280e694db9ed98b79e25406ff5efec43
Crhys <lg.christophe@gmail.com>
2016-03-05 18:42:15 +0100

    modification structure code

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongoose.connect(database.localUrl, function(err) {
   if (err) { throw err; }
 }); 	// Connect to local MongoDB instance. A remoteUrl is also available (modulus.io)
 
-app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
+app.use(express.static(__dirname)); 		// set the static files location /public/img will be /img for users
 app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
