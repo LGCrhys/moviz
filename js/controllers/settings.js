257bb7a7f3f7f780433fb478fbb71d46c03d6713
Crhys <lg.christophe@gmail.com>
2016-03-21 22:40:54 +0100

    centralisation contenu formulaire

--- js/controllers/settings.js
+++ js/controllers/settings.js
@@ -9,19 +9,29 @@
  */
 angular.module('movizApp')
   .controller('SettingsCtrl',['$scope', function($scope) {
-  	$scope.mediaType = 'movie';
-   	$scope.categories = {
-	    availableOptions: [
-      		{id: '1', name: 'Western' , media:'movie'},
-      		{id: '2', name: 'Biopics' , media:'movie'},
-      		{id: '3', name: 'Action' , media:'movie'},
-      		{id: '4', name: 'R&B' , media:'music'},
-      		{id: '5', name: 'Jazz' , media:'music'},
-      		{id: '6', name: 'Funk' , media:'music'},
-      		{id: '7', name: 'Trip' , media:'picture'},
-      		{id: '8', name: 'Party' , media:'picture'},
-      		{id: '9', name: 'Urban' , media:'picture'}
-    	],
-    	selectedOption: {id: '3', name: 'Action'} //This sets the default value of the select in the ui
-    };
-}]);
+	  	//TODO : Gestion categories via DB
+	   	$scope.categories = {
+		    availableOptions: [
+	      		{id: '1', name: 'Western' , media:'movie'},
+	      		{id: '2', name: 'Biopics' , media:'movie'},
+	      		{id: '3', name: 'Action' , media:'movie'},
+	      		{id: '4', name: 'R&B' , media:'music'},
+	      		{id: '5', name: 'Jazz' , media:'music'},
+	      		{id: '6', name: 'Funk' , media:'music'},
+	      		{id: '7', name: 'Trip' , media:'picture'},
+	      		{id: '8', name: 'Party' , media:'picture'},
+	      		{id: '9', name: 'Urban' , media:'picture'}
+	    	]};
+
+	    $scope.data = {
+	    	mediaType : 'movie',
+	    	title : '',
+	    	path : '',
+	    	cover_path : '',
+	    	categorie : ''
+	    }
+
+	    $scope.submit = function(){
+	    	console.log($scope.data);
+	    }
+	}]);
