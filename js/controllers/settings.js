72742f72deebc7fdbfe16ed80b4b5cee1e93bcf7
Crhys <lg.christophe@gmail.com>
2016-03-21 21:05:07 +0100

    gestion categorie avec ng-options

--- js/controllers/settings.js
+++ js/controllers/settings.js
@@ -8,5 +8,13 @@
  * Controller of the movizApp
  */
 angular.module('movizApp')
-  .controller('SettingsCtrl', function () {
-  });
+  .controller('SettingsCtrl',['$scope', function($scope) {
+   $scope.categories = {
+    availableOptions: [
+      {id: '1', name: 'Western'},
+      {id: '2', name: 'Biopics'},
+      {id: '3', name: 'Action'}
+    ],
+    selectedOption: {id: '3', name: 'Action'} //This sets the default value of the select in the ui
+    };
+}]);
