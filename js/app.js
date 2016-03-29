fefd16d978591a97f42fa31f76b5f5e6182b1f5a
Crhys <lg.christophe@gmail.com>
2016-03-29 17:12:20 +0200

    Ajout spinner pour les appel ajax

--- js/app.js
+++ js/app.js
@@ -41,4 +41,35 @@ angular
       .otherwise({
         redirectTo: '/'
       });
+  })
+  .config(function($httpProvider) {
+
+      $httpProvider.interceptors.push(function($q, $rootScope) {
+          return {
+              'request': function(config) {
+                  $rootScope.$broadcast('loading-started');
+                  return config || $q.when(config);
+              },
+              'response': function(response) {
+                  $rootScope.$broadcast('loading-complete');
+                  return response || $q.when(response);
+              }
+          };
+      });
+
+  })
+  .directive("loadingIndicator", function() {
+      return {
+          restrict : "A",
+          link : function(scope, element, attrs) {
+              scope.$on("loading-started", function(e) {
+                  element.css({"display" : ""});
+              });
+
+              scope.$on("loading-complete", function(e) {
+                  element.css({"display" : "none"});
+              });
+
+          }
+      };
   });
